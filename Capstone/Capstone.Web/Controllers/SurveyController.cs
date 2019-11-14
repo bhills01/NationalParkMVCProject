ed8ba41a67dcbb032574ed8d47ebb48add26e1a5
Joanna Cregan <joannamcregan@gmail.com>
2019-11-14 10:25:21 -0500

    all 50 states are stated

---
+++ Capstone/Capstone.Web/Controllers/SurveyController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Capstone.Web.DAL;
+using Capstone.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Capstone.Web.Controllers
+{
+    public class SurveyController : Controller
+    {
+        private IParksSQLDAO parkDAO;
+        private IWeatherSQLDAO weatherSQLDAO;
+
+        public SurveyController(IParksSQLDAO parkDAO, IWeatherSQLDAO weatherSQLDAO)
+        {
+            this.parkDAO = parkDAO;
+            this.weatherSQLDAO = weatherSQLDAO;
+        }
+        public IActionResult Index()
+        {
+            IList<Park> parks = parkDAO.GetParks();
+            return View(parks);
+        }
+    }
+}
---
+++ Capstone/Capstone.Web/Models/Survey.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Web.Models
+{
+    public class Survey
+    {
+        public string NameOfPark { get; set; }
+        public string Email { get; set; }
+        public string StateOfResidence { get; set; }
+        public string ActivityLevel { get; set; }
+    }
+}
